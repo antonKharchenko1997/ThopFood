9eb79a07fb7ca68426589e03edcc9287c58e60b7
Anton <55918247+anonymousStyleKiller@users.noreply.github.com>
2023-01-30 17:51:27 +0200

    style(web client ): add mud theme.

---
+++ ThopFood/Shared/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace ThopFood.Shared;
+
+public partial class MainLayout
+{
+    private  bool _drawerOpen = true;
+
+    public MudTheme MudTheme = new()
+    {
+        Palette = new Palette
+        {
+            Primary = "#8FC93A",
+            AppbarBackground = "#8FC93A",
+            Secondary = "#508CA4",
+            Background = "#F6FAFA",
+            BackgroundGrey = "#F0F7F4",
+        },
+    };
+
+    private void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }    
+}
