5dda5a33b6b1f2119828df8512871302ab5fb8ce
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-02-05 22:30:56 +0200

    feat(client tests): added first ui tests.

---
+++ ThopFood/Components/Recipe/RecipeTitleCardComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using ThopFood.Models;
+
+namespace ThopFood.Components.Recipe;
+
+public partial class RecipeTitleCardComponent
+{
+    private bool Loaded => Recipe != null;
+
+    [Parameter] public RecipeModel Recipe { get; set; }
+    [Parameter] public EventCallback OnRecipeChange { get; set; }
+
+    private async Task FavoriteClick()
+    {
+        Recipe.Favorite = !Recipe.Favorite;
+        await OnRecipeChange.InvokeAsync().ConfigureAwait(false);
+    }
+}
